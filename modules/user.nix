b928ab610d7f150335249378524be2eda1fb02c5
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-04-17 13:05:24 +0200

    add automatic deployment workflow

--- modules/user.nix
+++ modules/user.nix
@@ -3,15 +3,20 @@
     { config, ... }:
     {
       time.timeZone = config.primaryUser.timeZone;
-      users.users.${config.primaryUser.username} = {
-        isNormalUser = true;
-        description = "${config.primaryUser.firstName} ${config.primaryUser.lastName}";
-        extraGroups = [
-          "networkmanager"
-          "wheel"
-        ];
-        openssh.authorizedKeys.keys = config.primaryUser.publicSSHKeys;
-        initialPassword = "changeme";
+      users.users = {
+        ${config.primaryUser.username} = {
+          isNormalUser = true;
+          description = "${config.primaryUser.firstName} ${config.primaryUser.lastName}";
+          extraGroups = [
+            "networkmanager"
+            "wheel"
+          ];
+          openssh.authorizedKeys.keys = config.primaryUser.publicSSHKeys;
+          initialPassword = "changeme";
+        };
+        "root" = {
+          openssh.authorizedKeys.keys = config.primaryUser.publicSSHKeys;
+        };
       };
     };
 }
