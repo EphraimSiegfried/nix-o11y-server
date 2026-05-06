a25a61fdd3160b0ffcf58ca8294a1009ecdc8257
Ephraim Siegfried <ephraim.siegfried@proton.me>
2026-05-06 08:49:09 +0200

    flake update

--- modules/services/caddy.nix
+++ modules/services/caddy.nix
@@ -31,7 +31,7 @@
         enable = true;
         package = pkgs.caddy.withPlugins {
           plugins = [ "github.com/caddy-dns/cloudflare@v0.2.4" ];
-          hash = "sha256-4WF7tIx8d6O/Bd0q9GhMch8lS3nlR5N3Zg4ApA3hrKw=";
+          hash = "sha256-uKtStb6m1/hA5IaAdIyLGzAQdyIySjISdxXIRxehhyI=";
         };
         email = conf.primaryUser.email;
         globalConfig = ''
